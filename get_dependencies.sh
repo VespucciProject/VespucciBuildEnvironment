c297e10f976a91d4206444b704138743345f1a37
Daniel Foose <dpfoose@gmail.com>
2017-04-03 13:03:38 -0400

    added installation of dependencies

---
+++ get_dependencies.sh
@@ -0,0 +1,36 @@
+apt-get -y update
+apt-get -y install --no-install-recommends \
+    apt-utils \
+    git \
+    curl \
+    liblapack-dev \
+    build-essential \
+    libopenblas-dev \
+    cmake \
+    libhdf5-dev \
+    libxml2-dev \
+    libsz2 \
+    libboost-all-dev
+mkdir -p /build/dependencies
+cd /build/dependencies
+
+mkdir /src/build
+
+chmod +x linuxdeployqt
+cp linuxdeployqt /usr/bin
+
+# compile armadillo
+curl https://github.com/dpfoose/numerical-environment/releases/download/1/armadillo-7.800.1.tar.xz > armadillo.tar.xz
+tar xvf armadillo.tar.xz
+mkdir armadillo-7.800.1/build && cd armadillo-7.800.1/build
+cmake ..
+make && make install
+cd ../..
+
+# compile mlpack
+curl http://mlpack.org/files/mlpack-2.1.1.tar.gz > mlpack.tar.gz
+tar xvf mlpack.tar.gz
+mkdir mlpack-2.1.1/build && cd mlpack-2.1.1/build
+cmake .. -DBUILD_TESTS:BOOL=OFF -DBUILD_CLI_EXECUTABLES:BOOL=OFF
+make && make install
+cd ../..
